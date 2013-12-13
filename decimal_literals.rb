dd1a9e45bdcd271fc8e6c31244c7a021bd9f2893
Serdar <dogruyolserdar@gmail.com>
2013-12-13 11:12:46 +0200

    Fixed typo

--- decimal_literals.rb
+++ decimal_literals.rb
@@ -6,7 +6,7 @@ decimal =  0.1
 puts rational
 # (1/10)
 puts decimal 
-# (0.1)
+# (0.1l)
 
 puts rational == decimal
 # True
@@ -15,7 +15,7 @@ puts rational * 3 == decimal * 3
 
 Benchmark.bm do |x|
 	x.report("with suffix") { for i in 1..1000000; a = 0.1r; end }
-	x.report("without suffix") { for i in 1..100000; a = 0.1; end }
+	x.report("without suffix") { for i in 1..1000000; a = 0.1; end }
 end
-# Seems like r suffix is 10 times slower
+# Seems like they are the same in performance wise
 
