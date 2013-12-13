99c45ceda83e11f7c5e5a8f7b35b690c57643947
Serdar <dogruyolserdar@gmail.com>
2013-12-13 10:58:46 +0200

    Added benchmark for decimal_literals

--- decimal_literals.rb
+++ decimal_literals.rb
@@ -1,3 +1,5 @@
+require 'benchmark'
+
 rational = 0.1r
 decimal =  0.1
 
@@ -5,3 +7,10 @@ puts rational == decimal
 # True
 puts rational * 3 == decimal * 3
 # False
+
+Benchmark.bm do |x|
+	x.report("with suffix") { for i in 1..1000000; a = 0.1r; end }
+	x.report("without suffix") { for i in 1..100000; a = 0.1; end }
+end
+# Seems like r suffix is 10 times slower
+
